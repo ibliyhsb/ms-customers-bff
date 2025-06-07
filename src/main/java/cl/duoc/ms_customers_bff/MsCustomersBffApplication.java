c9ce3f97a3fe8f863d4b35f9f825013bedcb7d52
ibliyhsb <catd.rojas@duocuc.cl>
2025-06-07 13:39:27 -0400

    ms-customers-bff

---
+++ src/main/java/cl/duoc/ms_customers_bff/MsCustomersBffApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_customers_bff;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCustomersBffApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCustomersBffApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/ms_customers_bff/MsCustomersBffApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.ms_customers_bff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCustomersBffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
